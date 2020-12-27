bce05625e90fcee5f8dabb3f027e2cf43e9d080f
Calum Tomeny <939917+calumtomeny@users.noreply.github.com>
2020-12-27 12:32:23 +0000

    tests(webapp): mock useLocation to allow tests to pass

--- quizmaster-client-app/src/app/layout/Layout.test.tsx
+++ quizmaster-client-app/src/app/layout/Layout.test.tsx
@@ -4,6 +4,13 @@ import "@testing-library/jest-dom/extend-expect";
 import Layout from "./Layout";
 import ReactDOM from "react-dom";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "localhost:3000",
+  }),
+}));
+
 describe("When testing directly", () => {
   afterEach(cleanup);
   it("renders without crashing", () => {
