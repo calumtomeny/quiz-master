f7bf2b9cf60f4024cf1178a38b37dccf616e4e2d
someone <939917+calumtomeny@users.noreply.github.com>
2021-01-05 22:37:29 +0000

    fix(webapp): stop participant's connection from reconnecting if it is lost - rely on the helpful modal for the user to explicitly refresh

--- quizmaster-client-app/src/components/QuizParticipating/QuestionResponder.tsx
+++ quizmaster-client-app/src/components/QuizParticipating/QuestionResponder.tsx
@@ -178,18 +178,6 @@ export default function QuestionResponder() {
     const createHubConnection = async () => {
       // Build new Hub Connection, url is currently hard coded.
       const hubConnect = new HubConnectionBuilder()
-        .withAutomaticReconnect([
-          1000,
-          1000,
-          1000,
-          1000,
-          1000,
-          1000,
-          1000,
-          1000,
-          1000,
-          1000,
-        ])
         .withUrl(process.env.REACT_APP_BASE_API_URL + "/quiz")
         .configureLogging("trace")
         .build();
