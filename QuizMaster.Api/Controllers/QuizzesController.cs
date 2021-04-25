1f4d25a480a4426e0b0be1ff19f724a1df4eb554
Calum Tomeny <939917+calumtomeny@users.noreply.github.com>
2021-04-25 18:05:27 +0100

    146-feat(webapp): add participant list to participant lobby

--- QuizMaster.Api/Controllers/QuizzesController.cs
+++ QuizMaster.Api/Controllers/QuizzesController.cs
@@ -97,7 +97,6 @@ namespace TodoApi.Controllers
             });
         }
 
-        [ServiceFilter(typeof(ApiKeyAuthAttribute))]
         [HttpGet("{id}/contestants")]
         public async Task<ActionResult<Contestant>> GetContestants(string id)
         {
---
+++ quizmaster-client-app/src/components/QuizParticipating/ParticipantLobby.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import ReactDOM from "react-dom";
+import ParticipantLobby from "./ParticipantLobby";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({
+    id: "1234",
+  }),
+  useRouteMatch: () => ({
+    url: "/quiz/f37ff3b7-284f-4915-81dd-f1edce15acb4/setup/",
+  }),
+}));
+
+describe("When testing directly", () => {
+  afterEach(cleanup);
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<ParticipantLobby />, div);
+  });
+});
